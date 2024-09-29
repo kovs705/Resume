b3fc0376b4a5b715596610c8fb56f6898066ab38
Eugene Kovs <smartkovsik@gmail.com>
2024-09-29 19:49:16 +0900

    Updated buttons, some small info

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var lang = navigator.language || navigator.userLanguage;
+    newChangeLanguage(lang);
 
-var rainbowColors = ['#FF7F00', '#0000FF', '#4B0082', '#8B00FF'];
-var skillSetItems = document.querySelectorAll('.skill-set li');
+    var rainbowColors = [
+        '#FF7F00', '#5858fa', '#8b48bb', '#37b63c',
+        '#FF0000', '#1f95c0', '#713cda', '#348686',
+        '#b62a76', '#FFD700', '#ff7303', '#FF4500'
+    ];
+    var skillSetItems = document.querySelectorAll('.skill-set li');
 
-for (var i = 0; i < skillSetItems.length; i++) {
-    
-    skillSetItems[i].onmouseover = function(e) {
-        var color = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
-        this.style.backgroundColor = color;
-    } 
+    skillSetItems.forEach(function(item) {
+        item.addEventListener('mouseover', function() {
+            var color = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
+            this.style.backgroundColor = color;
+        });
 
-    skillSetItems[i].onmouseout = function(e) {
-        this.style.backgroundColor = '';
-    }
-
-    // skillSetItems[i].onclick = function(e) {
-    //     var link = this.querySelector('a');
-    //     if (link) {
-    //         window.location = link.href;
-    //     }
-    // }
-}
+        item.addEventListener('mouseout', function() {
+            this.style.backgroundColor = '';
+        });
+    });
+});
