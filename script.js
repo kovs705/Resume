f8055eefd5155646e8cadec89a47085493cc626e
Eugene Kovs <smartkovsik@gmail.com>
2025-01-11 16:42:59 +0900

    Resume 2.0!!!

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     var lang = navigator.language || navigator.userLanguage;
     newChangeLanguage(lang);
+});
 
-    var rainbowColors = [
-        '#FF7F00', '#5858fa', '#8b48bb', '#37b63c',
-        '#FF0000', '#1f95c0', '#713cda', '#348686',
-        '#b62a76', '#FFD700', '#ff7303', '#FF4500'
-    ];
-    var skillSetItems = document.querySelectorAll('.skill-set li');
-
-    skillSetItems.forEach(function(item) {
-        item.addEventListener('mouseover', function() {
-            var color = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
-            this.style.backgroundColor = color;
-        });
-
-        item.addEventListener('mouseout', function() {
-            this.style.backgroundColor = '';
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
         });
     });
 });
